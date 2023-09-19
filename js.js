d534ac704ef3f87d731dbf8b733bd5c4ac3a539f
unknown <Baxtiyorddushaev@gmail.com>
2023-09-19 10:00:42 +0500

    first

---
+++ js.js
@@ -0,0 +1,27 @@
+
+
+var names = prompt('Kak vas zovut?', '');
+var age = +prompt('Skolko vam let?', '');
+var primerOne = +prompt('Reshite primer 6 + 3 = ?', '');
+var primerSec = +prompt('Reshite primer 20 - 5 = ?', '');
+var primerThree = +prompt('Reshite primer 15 + 2 = ?', '');
+var primerFourth = +prompt('Reshite primer 8 / 4 = ?', '');
+var primerFivth = +prompt('Reshite primer 10 % 3 = ?', '');
+var view = alert('Otkroyte console dlya prosmotra!');
+
+
+
+console.log('Menya zovut ' + names);
+console.log('Mne ' + age, 'let');
+console.log('Primer 1: 6 + 3 = 9 ' + 'Vash otvet', primerOne);
+console.log('Primer 2: 20 - 5 = 15 ' + 'Vash otvet', primerSec);
+console.log('Primer 3: 15 * 2 = 30 ' + 'Vash otvet', primerThree);
+console.log('Primer 4: 8 / 4 = 2 ' + 'Vash otvet', primerFourth);
+console.log('Primer 5: 10 % 3 = 1 ' + 'Vash otvet', primerFivth);
+
+
+
+
+
+
+
